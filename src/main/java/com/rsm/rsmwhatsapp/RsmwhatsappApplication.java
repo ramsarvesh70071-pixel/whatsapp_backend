db34084345b83bf4a9928586e881d7466f5fcd8a
Ramsarvesh Maurya <ramsarvesh70071@gmail.com>
2026-01-05 21:24:48 +0530

    Initial commit - Project Started

---
+++ src/main/java/com/rsm/rsmwhatsapp/RsmwhatsappApplication.java
@@ -0,0 +1,13 @@
+package com.rsm.rsmwhatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsmwhatsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsmwhatsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsm/rsmwhatsapp/RsmwhatsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsm.rsmwhatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsmwhatsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
