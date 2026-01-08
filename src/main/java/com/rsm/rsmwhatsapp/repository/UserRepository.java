c44788e86e3889bf003bdfacb1af49cde97d89fc
Ramsarvesh Maurya <ramsarvesh70071@gmail.com>
2026-01-08 18:41:42 +0530

    Final sync of models and services to fix null formatting and missing members

--- src/main/java/com/rsm/rsmwhatsapp/repository/UserRepository.java
+++ src/main/java/com/rsm/rsmwhatsapp/repository/UserRepository.java
@@ -2,15 +2,9 @@ package com.rsm.rsmwhatsapp.repository;
 
 import com.rsm.rsmwhatsapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> { // String ki jagah Long (ID numeric hai)
-
-    // 1. Online status ke hisaab se users dhoondna
+public interface UserRepository extends JpaRepository<User, String> {
     List<User> findAllByOnline(boolean online);
-
-    // 2. FIXED: Phone number se search karne ke liye (Login/Register ke liye zaroori hai)
-    User findByPhoneNumber(String phoneNumber);
+    // Phone number se search karne ke liye defaultfindById() kaafi hai
 }
